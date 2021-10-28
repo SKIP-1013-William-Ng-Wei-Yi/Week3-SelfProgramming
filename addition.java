d8a6ddb17a2fcac5006173e609060bb5895900f1
William Ng Wei Yi <92504689+William284613@users.noreply.github.com>
2021-10-29 00:04:56 +0800

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,11 @@
+package week3_Part1;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// Concatenates and adds two numbers and prints the results.
+		System.out.println("24 and 45 concatenated : " +24 +45); // not in () willl shown as string characters
+		System.out.println("24 and 45 added : " + (24+45) ); //numeric which in () only will be defined as addition
+	}
+
+}
