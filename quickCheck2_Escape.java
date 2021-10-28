cef44fee19539aba8f9d86cfa4d0d8d961a2e503
William Ng Wei Yi <92504689+William284613@users.noreply.github.com>
2021-10-29 00:09:09 +0800

    Create quickCheck2_Escape.java

---
+++ quickCheck2_Escape.java
@@ -0,0 +1,17 @@
+package week3_Part1;
+
+public class quickCheck2_Escape {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//myCode
+		System.out.println ("\"Thank you all for coming to my home\n" + "tonight,\" he said mysteriously." );
+		//noteCode
+		System.out.println("");
+		System.out.println ("\"Thank you all for " +
+				   "coming to my home\ntonight,\" he said " +
+				   "mysteriously.");
+
+	}
+
+}
